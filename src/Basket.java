13f26412c1bd793930d980807699b57898f792b2
Lenar Khafizov <lenarkh@mail.ru>
2022-12-11 21:17:16 +0300

    Задача 1 выполнена.

--- src/Basket.java
+++ src/Basket.java
@@ -1,18 +1,29 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import static com.sun.org.apache.xml.internal.utils.XMLCharacterRecognizer.isWhiteSpace;
+
 public class Basket {
     private String[] products;
     private int[] prices;
     private String[] units;
-    private int[] counts ={0, 0, 0, 0};
+    private int[] counts = {0, 0, 0, 0};
 
-    public Basket (String[] products, int[] prices, String[] units) {
+    public Basket(String[] products, int[] prices, String[] units) {
         this.products = products;
         this.prices = prices;
         this.units = units;
     }
-    public void addToCart (int productNum, int amount) {
-        int i = productNum-1;
+    public int getCounts(int i) {
+        return counts[i];
+    }
+
+    public void addToCart(int productNum, int amount) {
+        int i = productNum - 1;
         counts[i] = counts[i] + amount;
     }
+
     public void printCart() {
         int productSumAll = 0;
         System.out.println("Ваша корзина:");
@@ -24,5 +35,33 @@ public class Basket {
             }
         }
         System.out.println("Общая сумма покупки: " + productSumAll + " руб.");
+    }
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            byte[] bytes = new byte[counts.length];
+            for (int e = 0; e < counts.length; e++) {
+                bytes[e]= (byte) counts[e];
+                out.print(bytes[e] + " ");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    public void loadFromTxtFile(File textFile) throws IOException {
+        try (FileInputStream f = new FileInputStream(textFile)) {
+            byte[] bytes = new byte[(char)textFile.length()];
+            f.read(bytes);
+            String inputFromFile = "";
+            for (int i = 0; i < bytes.length; i++) {
+                char s = (char) bytes[i];
+                inputFromFile = inputFromFile + s; //
+            }
+            String[] parts = inputFromFile.split(" ");
+            for (int i = 0; i < parts.length; i++) {
+                counts[i] = Integer.parseInt(parts[i]);
+                }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,19 @@ public class Main {
         String[] products = {"Хлеб", "Молоко", "Гречневая крупа", "Яблоки"};
         int[] prices = {55, 79, 95, 110};
         String[] units ={"буханка", "литр", "кг", "кг"};
+        Basket basket = new Basket(products, prices, units);
         File textFile = new File("basket.txt");
-        if (!textFile.exists()) textFile.createNewFile();
-
-
+        if (!textFile.exists()) {
+            textFile.createNewFile();
+        } else {
+            basket.loadFromTxtFile(textFile);
+        }
         System.out.println("Список товаров, доступных для покупки:");
         for (int i = 0; i < products.length; i++) {
             int j = i + 1;
             System.out.println(j + ". " + products[i] + " - " + prices[i] + " рублей/" + units[i]+ ".");
         }
         System.out.println();
-        Basket basket = new Basket(products, prices, units);
         while (true) {
             System.out.println("Выберите товар, введите его номер и количество либо введите 'end' для завершения:");
             String inputString = scanner.nextLine();
@@ -31,5 +33,6 @@ public class Main {
             basket.addToCart(productNumber, productCount);
         }
         basket.printCart();
+        basket.saveTxt(textFile);
     }
 }
